ef486e853eb073b1135c291b37a10defb173ec8d
Aria Andika <andikaakun17@gmail.com>
2022-07-17 09:52:09 +0700

    Summit starter packaging

---
+++ Packing/PublicData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace deuzo.core
+{
+    public class PublicData : MonoBehaviour
+    {
+        public static string Version = "1.0.0";
+    }
+}
